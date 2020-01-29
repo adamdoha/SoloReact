258c941100d888b62cb8f8f3a45247ec68ac9cba
Dolphago <37108728+adamdoha@users.noreply.github.com>
2020-01-29 17:29:54 +0900

    React-css-독학

---
+++ styling-react/src/App.js
@@ -0,0 +1,39 @@
+// import React from "react";
+// import logo from "./logo.svg";
+// import "./App.css";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header>
+//         <img src={logo} className="logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+import React, { Component } from "react";
+import SassComponent from "./SassComponent";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SassComponent />
+      </div>
+    );
+  }
+}
+
+export default App;
