e0b028c1f45e033dcb2b4292530f48ac15724f6d
Dolphago <37108728+adamdoha@users.noreply.github.com>
2020-02-03 13:34:37 +0900

    todolist 완성

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,61 +1,85 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useReducer, useRef, useCallback } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
-const App = () => {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: '리액트의 기초 알아보기',
-      checked: true,
-    },
-    {
-      id: 2,
-      text: '컴포넌트 스타일링 해보기',
-      checked: true,
-    },
-    {
-      id: 3,
-      text: '일정 관리 앱 만들어 보기',
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
       checked: false,
-    },
-  ]);
+    });
+  }
+  return array;
+}
 
+function todoReducer(todos, action) {
+  switch (action.type) {
+    case 'INSERT': //새로 추가
+      return todos.concat(action.todo);
+    case 'REMOVE':
+      return todos.filter(todo => todo.id !== action.id);
+    case 'TOGGLE':
+      return todos.map(todo =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo,
+      );
+    default:
+      return todos;
+  }
+}
+
+const App = () => {
+  // const [todos, setTodos] = useState([
+  //   {
+  //     id: 1,
+  //     text: '리액트의 기초 알아보기',
+  //     checked: true,
+  //   },
+  //   {
+  //     id: 2,
+  //     text: '컴포넌트 스타일링 해보기',
+  //     checked: true,
+  //   },
+  //   {
+  //     id: 3,
+  //     text: '일정 관리 앱 만들어 보기',
+  //     checked: false,
+  //   },
+  // ]);
+  // const [todos, setTodos] = useState(createBulkTodos);
+  //함수형으로 createBulkTodos() 라고 하면 리렌더링 될 때마다 createBulkTodos함수가 호출되지만,
+  // 파라미터를 함수형태로 넣어주면 컴포넌트가 처음 렌더링될 떼만 createBulkTodos 함수가 실행된다.
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
   //고유값으로 사용될 id
   //ref를 사용하여 변수 담기
-  const nextId = useRef(4);
+  const nextId = useRef(2501);
 
-  const onInsert = useCallback(
-    text => {
-      const todo = {
-        id: nextId.current,
-        text,
-        checked: false,
-      };
-      setTodos(todos.concat(todo));
-      nextId.current += 1; //nextId 1씩 더하기
-    },
-    [todos],
-  );
+  const onInsert = useCallback(text => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false,
+    };
+    dispatch({ type: 'INSERT', todo });
+    // setTodos(todos => todos.concat(todo));
+    nextId.current += 1; //nextId 1씩 더하기
+  }, []);
 
-  const onRemove = useCallback(
-    id => {
-      setTodos(todos.filter(todo => todo.id !== id));
-    },
-    [todos],
-  );
+  const onRemove = useCallback(id => {
+    dispatch({ type: 'REMOVE', id });
+    // setTodos(todos => todos.filter(todo => todo.id !== id));
+  }, []);
 
-  const onToggle = useCallback(
-    id => {
-      setTodos(
-        todos.map(todo =>
-          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
-        ),
-      );
-    },
-    [todos],
-  );
+  const onToggle = useCallback(id => {
+    // setTodos(todos =>
+    //   todos.map(todo =>
+    //     todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+    //   ),
+    // );
+    dispatch({ type: 'TOGGLE', id });
+  }, []);
 
   return (
     <TodoTemplate>
--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
 import './TodoList.scss';
 import TodoListItem from './TodoListItem';
+import { List } from 'react-virtualized';
 
 const TodoList = ({ todos, onRemove, onToggle }) => {
-  return (
-    <div className="TodoList">
-      {todos.map(todo => (
+  //이 함수는 react-virtualized의 List컴포넌트에서 각 TodoItem을 렌더링할 때 사용하며, 이 함수를 List컴포넌트의 props로 설정해주어야 합닝다.
+  //이 함수는 파라미터에 index,key,style 값을 객체 타입으로 받아와서 사용합니다.
+  const rowRenderer = useCallback(
+    ({ index, key, style }) => {
+      const todo = todos[index];
+      return (
         <TodoListItem
           todo={todo}
-          key={todo.id}
+          key={key}
           onRemove={onRemove}
           onToggle={onToggle}
+          style={style}
         />
-      ))}
-    </div>
+      );
+    },
+    [onRemove, onToggle, todos],
+  );
+  return (
+    <List
+      className="TodoList"
+      width={512} //전체 크기
+      height={513} //전체 높이
+      rowCount={todos.length} //항목 개수
+      rowHeight={56} //항목 높이
+      rowRenderer={rowRenderer} //항목을 렌더링할 때 쓰는 함수
+      list={todos} //배열
+      style={{ outline: 'none' }} //List에 기본적용되는 outline스타일 제거
+    />
   );
 };
 
-export default TodoList;
+export default React.memo(TodoList);
