3acb155feb0240fb10a6f6e0bdad1f9b54a0f0b4
Dolphago <adamdoha@naver.com>
2020-05-03 20:12:24 +0900

    modal test

--- react-test-app/src/App.js
+++ react-test-app/src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Modal from "react-modal";
 
 function App() {
+  var subtitle;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    subtitle.style.color = "#00ff14";
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={openModal}>Click here to use Modal!</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        contentLabel="Example Modal"
+      >
+        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>
+          안녕하세요! Dolphago입니다.
+        </h2>
+        <button onClick={closeModal}>닫기</button>
+        <div>모달창에 들어왔습니다</div>
+        <form>
+          <input type="text" defaultValue="DolphaGo" />
+          <button>기능추가</button>
+          <button>기능추가</button>
+          <button>기능추가</button>
+          <button>기능추가</button>
+        </form>
+      </Modal>
     </div>
   );
 }
